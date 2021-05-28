b591be6ce562d90d49a4bf63e1ec71840d873c92
gatiz1m0 <orlandroide@gmail.com>
2021-05-28 01:16:32 -0700

    It can read file paths ok now

--- xmljsonconverter/src/main/java/tools/xmljsonconverter/XmlToJson.java
+++ xmljsonconverter/src/main/java/tools/xmljsonconverter/XmlToJson.java
@@ -1,8 +1,10 @@
 package tools.xmljsonconverter;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class XmlToJson {
 	
-	public void xmlReader( ) {
-		String jsonFile = " ";
-	}
+	
 }
